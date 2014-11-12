00a393741ec6f68e9b6f41ef5959fcc3856fdad1
webdagger <ayomipoe@gmail.com>
2014-11-12 17:47:17 +0100

    Create setup.sh
    Basic Install meh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#       QuickFix Mista X
+#       email: mistax@todaytoday.info
+#       site: http://www.todaytoday.info/mista-x
+clear
+#apt-get update && apt-get upgrade -y
+echo "Detecting Arch For Openvpn Access Download"
+arch=$(uname -m)
+if [ "$arch" == 'i386' ]
+then
+    echo "i386"
+    wget http://swupdate.openvpn.org/as/openvpn-as-2.0.10-Ubuntu14.amd_64.deb
+    dpkg -i openvpn-as-2.0.10-Ubuntu14.amd_64.deb
+else
+    echo "i686"
+    wget http://swupdate.openvpn.org/as/openvpn-as-2.0.10-Ubuntu14.i386.deb
+    dpkg -i openvpn-as-2.0.10-Ubuntu14.i386.deb
+fi
